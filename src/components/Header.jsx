7b9e34e40f36d91a793b8768dcfb1438b34b8367
Julian <jjvll90@gmail.com>
2024-10-14 20:45:55 -0500

    added bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <Navbar>
-            <Navbar.Brand href="#home">Love Garden Sounds</Navbar.Brand>
-            <Navbar.Toggle/>
-            <Navbar.Collapse>
-                <Nav>
-                    <Nav.Link>Home</Nav.Link>
-                    <Nav.Link>About</Nav.Link>
-                    <Nav.Link>Contact</Nav.Link>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Independent Record Store</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
-    )
+    );
 };
 
 export default Header;
